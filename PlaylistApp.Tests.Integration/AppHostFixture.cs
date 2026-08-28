7d9376a4530ccc68bd39e07e6345f6e125745f18
István Karsai <istvan.karsai.lfgeb@gmail.com>
2026-08-28 09:50:57 +0200

    fix(ci): trust dotnet dev-certs in ubuntu CA store

--- PlaylistApp.Tests.Integration/AppHostFixture.cs
+++ PlaylistApp.Tests.Integration/AppHostFixture.cs
@@ -28,7 +28,7 @@ public class AppHostFixture : IAsyncLifetime
 
         var healthUri = new Uri("/health", UriKind.Relative);
         int healthCheckAttempts = 0;
-        const int maxHealthCheckAttempts = 3_000;
+        const int maxHealthCheckAttempts = 1_000;
 
         // Wait for the API and EF Core Migrations to be 100% healthy
         while (healthCheckAttempts < maxHealthCheckAttempts)
@@ -48,7 +48,7 @@ public class AppHostFixture : IAsyncLifetime
 
         if (healthCheckAttempts >= maxHealthCheckAttempts)
         {
-            throw new TimeoutException("The API failed te report healthy within 300 seconds. Database or AppHost failed to boot.");
+            throw new TimeoutException("The API failed te report healthy within 100 seconds. Database or AppHost failed to boot.");
         }
 
         // Create the Respawner ONCE after tables are guaranteed to exist
